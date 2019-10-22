b8afce119271014a27bdbed765aa7efb6901fb21
saeed <seedxlss@gmail.com>
2019-10-22 19:12:01 +0400

    i calculated the rythm style and made the a command if the player wins the dance battle it says (yay you gained xp) but still i didnt finish my xphandler script

---
+++ Assets/Scripts/StatsGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  A <see langword="static"/> class with methods (functions) for initialising or randomising the stats class.
+///  
+/// TODO:
+///     Initialise a stats instance with generated starting stats
+///     Handle the assignment of extra points or xp into an existing stats of a character
+///         - this is expected to be used by NPCs leveling up to match the player.
+/// </summary>
+public static class StatsGenerator
+{
+    
+    public static void InitialStats(Stats stats)
+    {
+        stats.luck = Random.Range(0, 10);
+        stats.xp = Random.Range(-6, 60);
+        stats.style = Random.Range(-1, 100);
+        stats.rhythm = 5;
+    }
+
+    public static void AssignUnusedPoints(Stats stats, int points)
+    {
+    }
+}
---
+++ Assets/Scripts/XPHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is responsible for converting a battle result into xp to be awarded to the player.
+/// 
+/// TODO:
+///     Respond to battle outcome with xp calculation based on;
+///         player win 
+///         how strong the win was
+///         stats/levels of the dancers involved
+///     Award the calculated XP to the player stats
+///     Raise the player level up event if needed
+/// </summary>
+public class XPHandler : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        GameEvents.OnBattleConclude += GainXP;
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    public void GainXP(BattleResultEventData data)
+    {
+        if (data.outcome > 0)
+        {
+            Debug.Log("YAAAAYY YOU GAINED XP");
+        }
+    }
+}
